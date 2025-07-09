8b2f3889e3efb101f664f9245d0e21604e3f6d0e
Martin Pagac <martin.pagac@gmail.com>
2025-07-09 21:33:32 +0200

    feat: Implement native-like non-scrolling layout for mobile app

--- src/components/layouts/AppLayout.tsx
+++ src/components/layouts/AppLayout.tsx
@@ -4,12 +4,15 @@ import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div className="min-h-screen relative">
+    <div className="h-screen flex flex-col bg-gray-50 relative">
+      {/* Background Layers */}
       <div className="absolute inset-0 bg-[url('/bg_pattern.jpg')] bg-fixed bg-cover bg-center opacity-85" />
       <div className="absolute inset-0 bg-white/50" />
-      <div className="relative z-10">
+
+      {/* Content Layers */}
+      <div className="relative z-10 flex flex-col flex-grow">
         <Header />
-        <main className="pt-16 pb-24">
+        <main className="flex-grow overflow-y-auto pb-20"> 
           <Outlet />
         </main>
         <BottomNav />
